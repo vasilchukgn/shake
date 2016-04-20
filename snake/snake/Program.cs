74ad54cbaa89d4adb9615aa6336810580bb2ea84
vasilchukgn <gregoryvk@mail.ru>
2016-04-20 18:02:49 +0700

    Добавляем конструктор точки

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -11,18 +11,12 @@ namespace snake
         static void Main(string[] args)
         {
          
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1, 3, '*');
+            Point p2 = new Point(4, 5, '#');
+
             p1.Draw();
-
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
             p2.Draw();
-
+            
             Console.ReadLine();
         }
     }
