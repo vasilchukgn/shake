2261bf2b80a18a0a0484eaad9aac9542a98b515c
vasilchukgn <gregoryvk@mail.ru>
2016-04-20 17:22:53 +0700

    Change 1

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,25 +10,8 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';            
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x1, y1, sym1);
-            Draw(x2, y2, sym2);
-
+            Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
-
     }
 }
