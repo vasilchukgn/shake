194d6c18bb1b4fbe377673b2d254bef829a881f1
vasilchukgn <gregoryvk@mail.ru>
2016-04-21 13:41:05 +0700

    Добавлен функционал появления и поедания еды.

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -31,15 +31,29 @@ namespace snake
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
 
+            FoodCreator foodCreator = new FoodCreator(120, 30, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
             while (true)
             {
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                System.Threading.Thread.Sleep(100);
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
-                }
-                System.Threading.Thread.Sleep(100);
-                snake.Move();
+                }                                
             }
             
             //Console.ReadLine();
