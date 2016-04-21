dc1f6ce65baf3da4945f8750fdbea2f411b33d8f
vasilchukgn <gregoryvk@mail.ru>
2016-04-21 11:41:57 +0700

    Добавлен класс Figure, как родитель для линий.

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,18 +10,31 @@ namespace snake
     {
         static void Main(string[] args)
         {
+            Console.SetWindowSize(120, 30);
+            
+            //Console.SetBufferSize(50, 25);
          
-            Point p1 = new Point(1, 3, '*');
+/*            Point p1 = new Point(1, 3, '*');
             Point p2 = new Point(4, 5, '#');
             
             p1.Draw();
             p2.Draw();
+*/
+//           Console.WriteLine(Console.BufferWidth);
+//            Console.WriteLine(Console.BufferHeight);
 
-            HorizontalLine hline = new HorizontalLine(5, 30, 8, '+');
-            hline.Draw();
+            HorizontalLine topLine = new HorizontalLine(0, Console.BufferWidth - 1, 0, '+');
+            HorizontalLine bottomLine = new HorizontalLine(0, Console.BufferWidth - 1, 28, '+');
+            VerticalLine leftLine = new VerticalLine(0, 28, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 28, Console.BufferWidth - 1, '+');
+            
+            topLine.Draw();            
+            bottomLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
 
-            VerticalLine vline = new VerticalLine(8, 20, 5, '+');
-            vline.Draw();
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
 
             Console.ReadLine();
         }
